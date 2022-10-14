8ca29748b306590965ed902f2ede03267f2f7174
root <quadrameen@gmail.com>
2022-10-14 14:29:32 -0700

    fixed errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,6 +17,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
@@ -25,5 +26,6 @@ int main(void)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is positive\n", n);
+
 	return (0);
 }
